a0e0d8e8cbb01daf99a7f89dbf6516b7ee1d200b
Abdi-Nunow <101399179+Abdi-Nunow@users.noreply.github.com>
2025-12-31 06:17:42 -0800

    app.py

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Firebase initialization
+# Firebase init ka akhrinaya Streamlit Secrets
 if not firebase_admin._apps:
     cred = credentials.Certificate(dict(st.secrets["firebase"]))
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
-st.set_page_config(page_title="Planning & Budget System", layout="wide")
 st.title("Planning & Budget Submission System")
 st.write("Fadlan isticmaal menu-ga bidix")
 
-# Access API_KEY if needed
-API_KEY = st.secrets["AIzaSyAQUJ6UIwmSulo2ewXBZojiRBzRH7LEdXw"]
+# Web API Key
+API_KEY = st.secrets["firebase_api_key"]
+
 
 
