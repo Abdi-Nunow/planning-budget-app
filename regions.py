e3b1f259d56654a5f2ceeff53ef077b77538e96a
Abdi-Nunow <101399179+Abdi-Nunow@users.noreply.github.com>
2025-12-31 04:43:44 -0800

    regions.py

---
+++ regions.py
@@ -0,0 +1,10 @@
+somali_region = {
+    "Fafan": ["Jigjiga","Tog Wajaale","Babile","Gursum","Awbare","Harshin","Kebri Beyah"],
+    "Sitti": ["Shinile","Erer","Dembel","Mieso","Afdem","Hadigala"],
+    "Jarar": ["Degehabur","Gashamo","Aware","Gunagado","Birqod"],
+    "Nogob": ["Fik","Segeg","Hamero","Lagahida","Meyumuluka"],
+    "Korahe": ["Kebri Dahar","Shilabo","Shekosh","Marsin","Dobaweyn"],
+    "Doollo": ["Gode","Wardheer","Bokh","Danot","Laas-Dhankayre"],
+    "Afder": ["Hargele","Barey","El Kere","God-God"],
+    "Liben": ["Filtu","Dolo Odo","Moyale","Bokolmayo"],
+    "Shabelle": ["Mustahil","Kelafo","Ferfer","East Imey","West Imey"]
